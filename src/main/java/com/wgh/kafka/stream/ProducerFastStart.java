9bf44d93cabf5d8a34b6c8f56c33c0974eecb95f
wgh <2535362015@qq.com>
2022-08-17 21:42:36 +0800

    kafka stream Test

---
+++ src/main/java/com/wgh/kafka/stream/ProducerFastStart.java
@@ -0,0 +1,55 @@
+package com.wgh.kafka.stream;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.protocol.types.Field;
+
+import java.util.Properties;
+
+/**
+ * 消息生产者
+ */
+public class ProducerFastStart {
+
+    private static final String INPUT_TOPIC="input_topic";
+
+    public static void main(String[] args) {
+
+        //添加kafka的配置信息
+        Properties properties = new Properties();
+        //配置broker信息
+        properties.put("bootstrap.servers","192.168.200.130:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.RETRIES_CONFIG,10);
+
+        //生产者对象
+        KafkaProducer<String,String> producer = new KafkaProducer<String, String>(properties);
+
+
+        try {
+            //封装消息
+            for (int i = 0; i < 10; i++) {
+                if (i % 2 == 0) {
+                    ProducerRecord<String,String> record =
+                            new ProducerRecord<String, String>(INPUT_TOPIC,i+"","hello shanghai kafka stream hello");
+                    //发送消息
+                    producer.send(record);
+                    System.out.println("发送消息："+record);
+                }else {
+                    ProducerRecord<String,String> record =
+                            new ProducerRecord<String, String>(INPUT_TOPIC,i+"","helloworld kafka stream");
+                    //发送消息
+                    producer.send(record);
+                    System.out.println("发送消息："+record);
+                }
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //关系消息通道
+        producer.close();
+
+    }
+}
